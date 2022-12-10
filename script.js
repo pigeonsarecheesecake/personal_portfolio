c4b9deee9e8c4d6fe7cc5b41405befcadd15ef32
William Ryuputra <williamryuputra@yahoo.co.id>
2022-12-10 12:10:05 -0500

    Adds javascript for slideshow

---
+++ script.js
@@ -0,0 +1,31 @@
+// JS for slides
+let index = 0;
+let slides = document.getElementsByClassName('slide');
+
+// Function for arrows to add index
+function addSlide(n){
+    index += n;
+    if (index > slides.length-1){
+        index = 0;
+        currentSlide(index);
+    }
+
+    else if (index < 0){
+        index = slides.length-1;
+        currentSlide(index);
+    }
+
+    else{
+        currentSlide(index);
+    }
+}
+
+// Shows Current Slide
+function currentSlide(){
+    for(i = 0 ; i < slides.length ; i++){
+        slides[i].style.display = 'none';
+    }
+    slides[index].style.display = 'flex';
+}
+
+currentSlide();
