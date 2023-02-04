4cf3136c858d3709006eae0d4ae668c8221f0916
William Ryuputra <williamryuputra@yahoo.co.id>
2023-02-03 21:26:21 -0500

    Adds light mode dark mode toggle

--- script.js
+++ script.js
@@ -34,7 +34,60 @@ function currentSlide(){
     }
  
 
+// JS for different mode
+// Home
+let ball = document.getElementById("circle");
+let newToggle = document.getElementsByClassName("toggle");
+let background = document.querySelector("body");
+let letter = document.querySelectorAll(".hello > span");
+let social = document.querySelectorAll(".socials svg");
+let fullName = document.querySelector(".desc > span");
+let description = document.getElementsByClassName("desc");
+let resume = document.getElementsByClassName("resume");
+let navigation = document.querySelectorAll(".navbar > a");
+
+// Project
+let projectPage = document.getElementById("projects");
+let firstDivider = document.querySelector(".divider-one svg");
+let secondDivider = document.querySelector(".divider-two svg");
+let slide = document.querySelectorAll('.img-container');
+
+ball.addEventListener("click", function(){
+    // Moving ball to the right and changes color between modes
+    ball.classList.toggle("toggle-right");
+    newToggle[0].classList.toggle("toggle-color-change");
+
+    // Home page
+    background.classList.toggle("white-background");
+    description[0].classList.toggle("colorchange");
+    resume[0].classList.toggle("resume-color-change");
+    fullName.classList.toggle("name-color-change");
+   
+    for(let i = 0; i<letter.length ; i++){
+        letter[i].classList.toggle("colorchange");
+        // console.log(letter[i]);
+    }
+
+    for(let i = 0; i<social.length ; i++){
+        social[i].classList.toggle("colorchange");
+        // console.log(social[i]);
+    }
+
+    for(let i = 0; i<navigation.length ; i++){
+        navigation[i].classList.toggle("colorchange");
+        // console.log(social[i]);
+    }
+
+    // Projects page
+    projectPage.classList.toggle("project-background-change");
+    firstDivider.classList.toggle("divider-one-black");
+    secondDivider.classList.toggle("divider-two-black");
+    for(i = 0 ; i<slide.length;i++){
+        slide[i].classList.toggle("border-white");
+    }
     
+});
+
     
 
 
